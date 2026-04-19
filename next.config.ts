99eef2135e607cf5670c505dcfc3a5a657186676
Rudra Nautiyal <rudra.x.nautiyal@gmail.com>
2026-04-19 18:56:11 +0530

    vapi fix, audio playback done

--- next.config.ts
+++ next.config.ts
@@ -14,7 +14,7 @@ const nextConfig: NextConfig = {
       { protocol: "https", hostname: "covers.openlibrary.org" },
       {
         protocol: "https",
-        hostname: "lspfdyhgsrgsxcju.public.blob.vercel-storage.com",
+        hostname: "*.public.blob.vercel-storage.com",
       },
     ],
   },
